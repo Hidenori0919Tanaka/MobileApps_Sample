b35444f4e23ab9319aa54e440c81a17ba6405a75
HIDENORI TANAKA <hidenori091985@yahoo.co.jp>
2018-08-22 09:17:30 +0900

    プロジェクト ファイルを追加します。

---
+++ MobileApps_Sample/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MobileApps_Sample.Startup))]
+
+namespace MobileApps_Sample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
